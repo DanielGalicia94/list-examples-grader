afea47d5984f4cc44db16156e2c8a025637a01ff
DanielGalicia94 <dgalicia04@hotmail.com>
2023-05-25 16:41:40 -0700

    yes

---
+++ Exit.java
@@ -0,0 +1,5 @@
+class Exit{
+    public static void main(String[] args) {
+       System.exit(1); 
+    }
+}
--- grade.sh
+++ grade.sh
@@ -1,13 +1,35 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
-
 rm -rf student-submission
 rm -rf grading-area
 
+
 mkdir grading-area
-
-git clone $1 student-submission
+# clpning the data
+git clone $1 student-submission 
+if [[ $? -gt 0 ]]
+then
+    echo 'No submission detected'
+    exit 1
+fi
 echo 'Finished cloning'
+# checking is file inserted is correct
+file=`find student-submission -name "*.java"`
 
+if [[ -f $file ]] && [[ $file == *ListExamples.java ]] 
+then
+    echo 'Correct file submitted'
+else 
+    echo 'File not found'
+    exit 1
+fi
+
+# copying files to correct directory
+cp $file grading-area
+cp TestListExamples.java grading-area
+cp -r lib grading-area
+
+# runing javac 
+cd gradind-area
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
